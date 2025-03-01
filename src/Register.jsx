d391a728db38e0d4134a9bd88957182a8cc815c6
tonyh2323 <tonyh2323@yahoo.com>
2025-03-01 11:09:37 -0600

    Added required to registered

--- src/Register.jsx
+++ src/Register.jsx
@@ -29,11 +29,11 @@ export const Register = (props) =>{
             <h2>Register</h2>
             <form className='register-form' onSubmit={handleSubmit} >
                 <label htmlFor='name'>Full Name</label>
-                <input value={name} onChange={(e) => setName(e.target.value)} name='name' id='name' placeholder='full Name'/>
+                <input required value={name} onChange={(e) => setName(e.target.value)} name='name' id='name' placeholder='full Name'/>
                 <label htmlFor='email'>email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youemail@gmail.com" id="email"name="email"/>
+                <input required value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youemail@gmail.com" id="email"name="email"/>
                 <label htmlFor='password'>A mixture of uppercase and lowercase letters, numbers, and special characters along with 8 units in length are required for the password.</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*****" id="password"name="password"/>
+                <input required value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*****" id="password"name="password"/>
                 <button type="Submit" onClick={PasswordValidation}>Register</button>
                 <button className="link-btn" onClick={() => navigate("/login")}>Already have an account? Login here.</button>
                 
