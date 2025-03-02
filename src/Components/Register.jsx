cc98ee3f678564ce81de72fc3998883ec8c1f2a6
tonyh2323 <tonyh2323@yahoo.com>
2025-03-02 10:32:03 -0600

    show hide password on register screen

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -10,7 +10,10 @@ export const Register = (props) =>{
         const [pass, setPass] = useState('');
         const [first, setfirst] = useState('');
         const [last, setlast] = useState('');
-
+        const [isShown, setIsSHown] = useState(false);
+        const togglePassword = () => {
+            setIsSHown((isShown) => !isShown);
+          };
         const navigate = useNavigate();
 
         const handleSubmit = (e) =>{
@@ -43,7 +46,17 @@ export const Register = (props) =>{
                 <label htmlFor='email'>email</label>
                 <input required value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youemail@gmail.com" id="email"name="email"/>
                 <label htmlFor='password'>A mixture of uppercase and lowercase letters, numbers, and special characters along with 8 units in length are required for the password.</label>
-                <input required value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*****" id="password"name="password"/>
+
+                <input required value={pass} onChange={(e) => setPass(e.target.value)} type={isShown ? "text" : "password"} placeholder="*****" id="password"name="password"/>
+                <div className="checkbox-container">
+                    <label htmlFor="checkbox">Show password?</label>
+                    <input
+                        id="checkbox"
+                        type="checkbox"
+                        checked={isShown}
+                        onChange={togglePassword}
+                    />
+                </div>
                 <button type="Submit" onClick={() => PasswordValidation(pass)}>Register</button>
                 <button className="link-btn" onClick={() => navigate("/login")}>Already have an account? Login here.</button>
                 
