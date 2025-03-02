4787bd23717e091089117cf7c26a096b0a8a6895
tonyh2323 <tonyh2323@yahoo.com>
2025-03-02 10:35:35 -0600

    added showpassword to login screen

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,6 +8,10 @@ import { useNavigate } from 'react-router-dom';
 export  const Login = (props) =>{
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [isShown, setIsSHown] = useState(false);
+    const togglePassword = () => {
+        setIsSHown((isShown) => !isShown);
+        };
 
     const navigate = useNavigate();
 
@@ -22,7 +26,17 @@ export  const Login = (props) =>{
                 <label htmlFor='email'>email</label>
                 <input required value={email}  onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email"name="email"/>
                 <label htmlFor='password'>password. </label>
-                <input required value={pass}  onChange={(e) => setPass(e.target.value)} type="password" placeholder="*****" id="password"name="password"/>
+
+                <input required value={pass} onChange={(e) => setPass(e.target.value)} type={isShown ? "text" : "password"} placeholder="*****" id="password"name="password"/>
+                <div className="checkbox-container">
+                    <label htmlFor="checkbox">Show password?</label>
+                    <input
+                        id="checkbox"
+                        type="checkbox"
+                        checked={isShown}
+                        onChange={togglePassword}
+                    />
+                </div>
                 <button type="submit" >Log in</button>
           
                 <button className="link-btn" onClick={() => navigate("/register")}> Don't have an account? Register here</button>
